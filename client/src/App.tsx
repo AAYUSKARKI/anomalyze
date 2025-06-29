553bbec3f38ca34e1db519b77827c581463fa149
aayus karki <karki.aayush2003@icloud.com>
2025-06-29 09:08:14 +0545

    refactor(app): adjust routing and state integration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import LandingPage from "./pages/LandingPage"
 import AuthPage from "./pages/AuthPage"
 import AppLayout from "./components/AppLayout"
 import DataIngestion from "./components/DataIngestion"
-
+import MultiChannelView from "./pages/MultiChannelView"
 function App() {
   return (
     <BrowserRouter>
@@ -12,6 +12,7 @@ function App() {
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/dashboard" element={<AppLayout />}>
         <Route index element={<DataIngestion />} />
+        <Route path="multichannel" element={<MultiChannelView />} />
         </Route>
       </Routes>
     </BrowserRouter>
